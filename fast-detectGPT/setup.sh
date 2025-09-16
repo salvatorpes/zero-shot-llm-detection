20578e572ab28d8f7f779b4cb648a38c8ad57983
KrystofBo <krystof.bobek1@gmail.com>
2025-09-16 16:52:32 +0200

    setup for the old man

--- fast-detectGPT/setup.sh
+++ fast-detectGPT/setup.sh
@@ -1,5 +1,8 @@
 conda remove -n fastdetectGPT --all -y
-conda create -n fastdetectGPT python=3.9 -y
+conda create -n fastdetectGPT python=3.8 -y
 conda activate fastdetectGPT
-pip install tokenizers
+pip install --upgrade pip
 pip install -r requirements.txt
+pip install huggingface_hub
+huggingface-cli login
+pip install -U "datasets>=2.16.0,<3" "fsspec>=2023.10.0" "huggingface_hub>=0.20.0" "pyarrow>=14,<17"
