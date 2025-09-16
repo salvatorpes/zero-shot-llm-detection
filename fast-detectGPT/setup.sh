f3bdf6908f4d11203e9221d7f30fa51a381511f1
Salvador <salvatorpes@gmail.com>
2025-09-16 18:23:33 +0200

    new sh file for snellius

--- fast-detectGPT/setup.sh
+++ fast-detectGPT/setup.sh
@@ -1,9 +1,9 @@
+# clean cache
+conda clean --all -y
 conda remove -n fastdetectGPT --all -y
 conda create -n fastdetectGPT python=3.8 -y
 conda activate fastdetectGPT
+conda clean --all -y
 pip install --upgrade pip
 pip install -r requirements.txt
-pip install huggingface_hub
-pip install -U "huggingface_hub[cli]"
-huggingface-cli login
 pip install -U "datasets>=2.16.0,<3" "fsspec>=2023.10.0" "huggingface_hub>=0.20.0" "pyarrow>=14,<17"
