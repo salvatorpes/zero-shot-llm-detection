ce67baf1336680539ac778da9b3fdd33fdd4a513
Salvador <salvatorpes@gmail.com>
2025-09-17 14:05:29 +0200

    setup and snellius.sh update

--- fast-detectGPT/setup.sh
+++ fast-detectGPT/setup.sh
@@ -7,3 +7,5 @@ conda clean --all -y
 pip install --upgrade pip
 pip install -r requirements.txt
 pip install -U "datasets>=2.16.0,<3" "fsspec>=2023.10.0" "huggingface_hub>=0.20.0" "pyarrow>=14,<17"
+pip install python-dotenv
+pip install huggingface_hub
--- fast-detectGPT/snellius.sh
+++ fast-detectGPT/snellius.sh
@@ -15,25 +15,30 @@ module load Anaconda3/2024.06-1
 # Your job starts in the directory where you call sbatch
 cd $HOME/DL4NLP-Group15/fast-detectGPT
 
-# run the setup script
-bash setup.sh
+# run the setup script (uncomment this once)
+# bash setup.sh 
 
 # activate your conda environment
 source activate fastdetectGPT
 
 # Login to Hugging Face if not already logged in
-pip install python-dotenv
+# Load .env file if it exists
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
 if [ ! -f "$HOME/.cache/huggingface/token" ]; then
     echo "Logging in to Hugging Face CLI..."
     if [ -z "$HUGGINGFACE_TOKEN" ]; then
         echo "Error: HUGGINGFACE_TOKEN not set in .env file"
         exit 1
     fi
-    huggingface-cli login --token "$HUGGINGFACE_TOKEN"
+    # $CONDA_PREFIX/bin/huggingface-cli login --token "$HUGGINGFACE_TOKEN"
+    hf auth login --token "$HUGGINGFACE_TOKEN"
 else
     echo "Already logged in to Hugging Face."
 fi
 
 
 # Run your code
-# bash main.sh
+bash main.sh
